40f880250e3412de1cd9e462b3b7cd7344914552
alex.collins <alex.collins@gamesys.co.uk>
2013-12-21 18:40:06 +0000

    added defaults

---
+++ src/test/java/com/alexecollins/testing/Example.java
@@ -0,0 +1,30 @@
+package com.alexecollins.testing;
+
+/**
+ * @author alex.collins
+ */
+public interface Example {
+
+    void voidValue();
+
+    int intValue();
+
+    long longValue();
+
+    float floatValue();
+
+    double doubleValue();
+
+    byte byteValue();
+
+    char charValue();
+
+    short shortValue();
+
+    boolean booleanValue();
+
+    Object objectValue();
+
+    Object[] arrayValue();
+
+}
